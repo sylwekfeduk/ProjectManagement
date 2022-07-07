846c7b99bb25c2c7c0d233d2da265ac8c3cb8236
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2022-07-07 13:17:02 +0200

    exercise 4

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,4 +1,4 @@
-CREATE TYPE sprintStatus AS ENUM ('Pending', 'In progress', 'Finished', 'Canceled')
+CREATE TYPE sprintStatus AS ENUM ('Pending', 'In progress', 'Finished', 'Canceled');
 CREATE TABLE sprint (
     id INTEGER NOT NULL AUTO_INCREMENT,
     startDate DATE NOT NULL,
@@ -6,21 +6,21 @@ CREATE TABLE sprint (
     description TEXT NOT NULL,
     status sprintStatus NOT NULL,
     PRIMARY KEY(id)
-)
-CREATE TYPE userStoryStatus AS ENUM ('To do', 'In progress', 'Review', 'Done')
+);
+CREATE TYPE userStoryStatus AS ENUM ('To do', 'In progress', 'Review', 'Done');
 CREATE TABLE userStory (
     id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
-    attachments BYTEA NOT NULL,
+    attachments BYTEA,
     storyPoints INTEGER NOT NULL,
     status userStoryStatus NOT NULL,
     PRIMARY KEY(id)
-)
+);
 CREATE TABLE sprint_userStory (
     sprint_id INTEGER NOT NULL,
     userStory_id INTEGER NOT NULL,
     PRIMARY KEY (sprint_id, userStory_id),
-    CONSTRAINTS fk_sprint FOREIGN KEY (sprint_id) REFERENCES sprint(id),
-    CONSTRAINTS fk_userStory FOREIGN KEY (userStory_id) REFERENCES userStory(id)
-)
+    CONSTRAINT fk_sprint FOREIGN KEY (sprint_id) REFERENCES sprint(id),
+    CONSTRAINT fk_userStory FOREIGN KEY (userStory_id) REFERENCES userStory(id)
+);
