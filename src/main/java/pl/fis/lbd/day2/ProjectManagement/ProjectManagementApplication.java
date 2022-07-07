6fc5d950e3b36cdc3b57f9ebf04764397e01fcd5
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2022-07-07 10:12:59 +0200

    init commit day2

---
+++ src/main/java/pl/fis/lbd/day2/ProjectManagement/ProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.fis.lbd.day2.ProjectManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementApplication.class, args);
+	}
+
+}
